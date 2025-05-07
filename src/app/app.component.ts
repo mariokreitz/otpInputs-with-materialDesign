21a31a25c726c67a2e0593ace6d89f57ae963555
Mario Kreitz <Mario.Kreitz@web.de>
2025-05-07 09:00:54 +0200

    Create basic layout in app component for displaying OTP input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { OtpInputComponent } from './otp-input/otp-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [OtpInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'otpInputs-with-materialDesign';
